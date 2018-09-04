06a94875f4504c4482ab6f994e522ea22ff1ccba
commit <275519338@qq.com>
2018-09-04 17:44:20 +0800

    Update up.sh

--- scripts/sh/up.sh
+++ scripts/sh/up.sh
@@ -5,9 +5,9 @@ if [ $1 == "test" ]
     cd output/bak
     dirdate=`date +%Y%m%d`_`date +%H%M%S`
     mkdir $dirdate
-    #scp -r appdeploy@10.204.56.75:/app/nginx/html/mall.xxx.com/mall-sit2.fcbox.com/frontend $dirdate
+    #scp -r appdeploy@10.204.56.75:/app/nginx/html/mall.xxx.com/mall-sit2.xxx.com/frontend $dirdate
     cd ../
     rm -rf frontend
     mv test frontend
-    #scp -r frontend appdeploy@10.204.56.75:/app/nginx/html/mall.fcbox.com/mall-sit2.xxx.com/
+    #scp -r frontend appdeploy@10.204.56.75:/app/nginx/html/mall.xxx.com/mall-sit2.xxx.com/
 fi
